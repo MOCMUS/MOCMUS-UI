65127bef2ea72ab9aae2978e2ae5d971646cf456
mima2101 <mimault0221@hotmail.com>
2021-06-13 20:22:07 -0400

    added layout to ui

--- src/client/components/Layout.js
+++ src/client/components/Layout.js
@@ -1,34 +1,23 @@
 import React, { Component } from 'react';
 import { Grid } from '@material-ui/core';
 import { styles } from './../styles/theme'
-// import './styles/app.css';
 
 export default function Layout () {
 
   return (
-    <div>
+    <div style={styles.layoutContainer}>
         <Grid
-            style={styles.gridLayout}
+            style={styles.gridContainer}
             container
             container spacing={0}
-            justify="center"
-            alignItems="flex-start" 
             >
-            <Grid item xs={6}>
+            <Grid item style={styles.gridLeftItemLayer1} >
             </Grid>
-            <Grid item xs={6}>
-            </Grid>
-        </Grid>
-        <Grid
-            style={styles.gridLayout}
-            container
-            container spacing={0}
-            justify="center"
-            alignItems="flex-start" 
-            >
-            <Grid item xs={6}>
-            </Grid>
-            <Grid item xs={6}>
+            <Grid container style={styles.gridRightItemLayer1} >
+                <Grid item style={styles.gridTopItemLayer2} >
+                </Grid>
+                <Grid item style={styles.gridBottomItemLayer2} >
+                </Grid>
             </Grid>
         </Grid>
     </div>
--- src/client/styles/theme.js
+++ src/client/styles/theme.js
@@ -1,6 +1,34 @@
 export const styles = {
-    gridLayout:{
-        width:'50vw',
-        height:'50vh'
-    }
+
+    layoutContainer:{
+        width:'100vw',
+        height:'100vh'
+    },
+
+    gridContainer:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        width: '100%',
+        height: '100%'
+    },
+
+    gridLeftItemLayer1:{
+        flex: 1
+    },
+
+    gridRightItemLayer1:{
+        flex: 1,
+        flexDirection: 'column',
+        borderLeft: 'solid'
+    },
+
+    gridTopItemLayer2:{
+        flex: 1,
+        borderBottom: 'solid'
+    },
+
+    gridBottomItemLayer2:{
+        flex: 1
+    },
 }
