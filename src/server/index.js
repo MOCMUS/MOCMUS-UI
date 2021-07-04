32fbc49585679930076929e76d85d29b9b7d9cd1
mima2101 <mimault0221@hotmail.com>
2021-07-04 15:15:25 -0400

    imported serialport lib to server

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const os = require('os');
+const axios = require('axios');
 
 const app = express();
+
+var SerialPort = require("serialport");
+const arduinoCOMPort = "COM4";
+
+var arduinoSerialPort = new SerialPort(arduinoCOMPort, {  
+    baudRate: 115200,
+    parser: SerialPort.parsers.Readline("\n")
+   });
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
