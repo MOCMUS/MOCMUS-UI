e36dc0fc542ca774709b9dbf5936dfbffc03c163
mima2101 <mimault0221@hotmail.com>
2021-09-22 22:02:13 -0400

    added gcode parser to server

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const os = require('os');
+gcodeparser = require('gcode-parser')
 
 const app = express();
+
+gcodeparser.parseFile('gcode/circle.nc', function(err, result) {
+    const gcode = []
+    let isPaused = true
+    let fileIndex = 0
+    result.map((obj, ind) => gcode[ind] = obj.line)
+    console.log(gcode);
+
+    // var interval = setInterval(function(str1, str2) {
+    //     console.log(str1 + " " + str2);
+    //   }, 1000, "Hello.", "How are you?");
+})
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
