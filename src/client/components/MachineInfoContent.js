b5c288259c1b5c4ac943aff120b2e9bae2e0f388
mima2101 <mimault0221@hotmail.com>
2021-11-02 20:40:53 -0400

    fixed current info refresh for 5 axis

--- src/client/components/MachineInfoContent.js
+++ src/client/components/MachineInfoContent.js
@@ -6,33 +6,81 @@ import { styles } from './../styles/theme'
 
 export default function MachineInfoContent () {
 
-    const [machineState, setMachineState] = useState('')
-    const [MposX, setMposX] = useState('0.000')
-    const [MposY, setMposY] = useState('0.000')
-    const [MposZ, setMposZ] = useState('0.000')
-    const [MposB, setMposB] = useState('0.000')
-    const [MposC, setMposC] = useState('0.000')
-    const [WposX, setWposX] = useState('0.000')
-    const [WposY, setWposY] = useState('0.000')
-    const [WposZ, setWposZ] = useState('0.000')
-    const [WposB, setWposB] = useState('0.000')
-    const [WposC, setWposC] = useState('0.000')
+    const [machineState, setMachineState] = useState(sessionStorage.getItem('machine_state') ? sessionStorage.getItem('machine_state') : 'Idle')
+    const [MposX, setMposX] = useState(sessionStorage.getItem('MposX') ? sessionStorage.getItem('MposX') : '0.000')
+    const [MposY, setMposY] = useState(sessionStorage.getItem('MposY') ? sessionStorage.getItem('MposY') : '0.000')
+    const [MposZ, setMposZ] = useState(sessionStorage.getItem('MposZ') ? sessionStorage.getItem('MposZ') : '0.000')
+    const [MposB, setMposB] = useState(sessionStorage.getItem('MposB') ? sessionStorage.getItem('MposB') : '0.000')
+    const [MposC, setMposC] = useState(sessionStorage.getItem('MposC') ? sessionStorage.getItem('MposC') : '0.000')
+    const [WposX, setWposX] = useState(sessionStorage.getItem('WposX') ? sessionStorage.getItem('WposX') : '0.000')
+    const [WposY, setWposY] = useState(sessionStorage.getItem('WposY') ? sessionStorage.getItem('WposY') : '0.000')
+    const [WposZ, setWposZ] = useState(sessionStorage.getItem('WposZ') ? sessionStorage.getItem('WposZ') : '0.000')
+    const [WposB, setWposB] = useState(sessionStorage.getItem('WposB') ? sessionStorage.getItem('WposB') : '0.000')
+    const [WposC, setWposC] = useState(sessionStorage.getItem('WposC') ? sessionStorage.getItem('WposC') : '0.000')
 
+    const [feedrate, setFeedrate] = useState(sessionStorage.getItem('feedrate') ? sessionStorage.getItem('feedrate') : '0')
+    const [spindleSpeed, setSpindleSpeed] = useState(sessionStorage.getItem('spindle_speed') ? sessionStorage.getItem('spindle_speed') : '0')
+
+    const [feedOverride, setFeedOverride] = useState(sessionStorage.getItem('feed_override') ? sessionStorage.getItem('feed_override') : '100')
+    const [rapidOverride, setRapidOverride] = useState(sessionStorage.getItem('rapid_override') ? sessionStorage.getItem('rapid_override') : '100')
+    const [spindleSpeedOverride, setSpindleSpeedOverride] = useState(sessionStorage.getItem('spindle_speed_override') ? sessionStorage.getItem('spindle_speed_override') : '100')
+    
     const getPositions = async () => {
         try {
       const currentPositions = await Axios.get('/current-positions')
       if (typeof currentPositions.data !== 'string')
         return
       
-      const infoPositions = currentPositions.data.split(',')
+      const infoPositions = currentPositions.data.split('|')
 
       setMachineState(infoPositions[0])
-      setMposX(infoPositions[1].split(':')[1])
-      setMposY(infoPositions[2])
-      setMposZ(infoPositions[3])
-      setWposX(infoPositions[4].split(':')[1])
-      setWposY(infoPositions[5])
-      setWposZ(infoPositions[6])
+      sessionStorage.setItem('machine_state', infoPositions[0])
+
+      const MPosInfo = infoPositions.find(info => info.startsWith('MPos'))?.split(':')[1].split(',')
+      if (MPosInfo) {
+        setMposX(MPosInfo[0])
+        sessionStorage.setItem('MposX', MPosInfo[0])
+        setMposY(MPosInfo[1])
+        sessionStorage.setItem('MposY', MPosInfo[1])
+        setMposZ(MPosInfo[2])
+        sessionStorage.setItem('MposZ', MPosInfo[2])
+        setMposB(MPosInfo[3])
+        sessionStorage.setItem('MposB', MPosInfo[3])
+        setMposC(MPosInfo[4])
+        sessionStorage.setItem('MposC', MPosInfo[4])
+      }
+
+      const FSInfo = infoPositions.find(info => info.startsWith('FS'))?.split(':')[1].split(',')
+      if (FSInfo) {
+        setFeedrate(FSInfo[0])
+        sessionStorage.setItem('feedrate', FSInfo[0])
+        setSpindleSpeed(FSInfo[1])
+        sessionStorage.setItem('spindle_speed', FSInfo[1])
+      }
+
+      const WPosInfo = infoPositions.find(info => info.startsWith('WCO'))?.split(':')[1].split(',')
+      if (WPosInfo) {
+        setWposX(WPosInfo[0])
+        sessionStorage.setItem('WposX', WPosInfo[0])
+        setWposY(WPosInfo[1])
+        sessionStorage.setItem('WposY', WPosInfo[1])
+        setWposZ(WPosInfo[2])
+        sessionStorage.setItem('WposZ', WPosInfo[2])
+        setWposB(WPosInfo[3])
+        sessionStorage.setItem('WposB', WPosInfo[3])
+        setWposC(WPosInfo[4])
+        sessionStorage.setItem('WposC', WPosInfo[4])
+      }
+
+      const OvInfo = infoPositions.find(info => info.startsWith('Ov'))?.split(':')[1].split(',')
+      if (OvInfo) {
+        setFeedOverride(OvInfo[0])
+        sessionStorage.setItem('feed_override', OvInfo[0])
+        setRapidOverride(OvInfo[1])
+        sessionStorage.setItem('rapid_override', OvInfo[1])
+        setSpindleSpeedOverride(OvInfo[2])
+        sessionStorage.setItem('spindle_speed_override', OvInfo[2])
+      }
       
       
         
