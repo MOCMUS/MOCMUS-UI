c25335f139edf49fffa57be17625c22d08082166
mima2101 <mimault0221@hotmail.com>
2021-11-04 13:24:40 -0400

    added visual feature for 5 axis manual control

--- src/client/components/StepperIncrementContent.js
+++ src/client/components/StepperIncrementContent.js
@@ -1,9 +1,29 @@
-import React, { Component, Text } from 'react';
+import React, { Component } from 'react';
 import { Icon, Grid, Button, Paper, TextField } from '@material-ui/core';
-import { ChevronRight } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles'
+import { ChevronRight, Refresh } from '@material-ui/icons';
 import { styles } from './../styles/theme'
 
+const useStyles = makeStyles({
+    button:{
+        height: '6vh',
+        width: '8vw',
+        fontSize: '1.2vw',
+        backgroundColor: 'rgb(15, 76, 117)',
+        color: 'rgb(187, 225, 250)'
+    },
+    axisCard:{
+        textAlign: 'center',
+        alignSelf: 'center',
+        width: '7%',
+        fontSize: '3vh',
+        padding: 2,
+        backgroundColor: 'rgb(187, 225, 250)'
+    },
+  })
+
 export default function StepperIncrementContent () {
+    const classes = useStyles()
 
     return (
       <div style={styles.divInitContent}>
@@ -12,13 +32,15 @@ export default function StepperIncrementContent () {
             >
                 <Grid item style={styles.gridItemTitle}
                 >
-                  <h1 style={styles.title}>Jog Controls</h1>  
+                  <h1 style={styles.title}>Manual</h1>  
                 </Grid>
                 <Grid item style={{display: 'flex', paddingBottom: 10, justifyContent: 'space-around'}}
                 >
-                    <Paper style={{textAlign: 'center', width: '5%', fontSize: 30, padding: 2, backgroundColor: 'rgb(187, 225, 250)'}}>X</Paper>
-                    <Paper style={{textAlign: 'center', width: '5%', fontSize: 30, padding: 2, backgroundColor: 'rgb(187, 225, 250)'}}>Y</Paper>
-                    <Paper style={{textAlign: 'center', width: '5%', fontSize: 30, padding: 2, backgroundColor: 'rgb(187, 225, 250)'}}>Z</Paper>
+                    <Paper className={classes.axisCard}> X </Paper>
+                    <Paper className={classes.axisCard}> Y </Paper>
+                    <Paper className={classes.axisCard}> Z </Paper>
+                    <Paper className={classes.axisCard}> B </Paper>
+                    <Paper className={classes.axisCard}> C </Paper>
                 </Grid>
                 <Grid item style={{display: 'flex', flex: 1, justifyContent: 'space-around'}}
                 >
@@ -26,7 +48,7 @@ export default function StepperIncrementContent () {
                 <TextField
                     id="increment-field-X"
                     type="number"
-                    inputProps={{min: '0', max: '50'}}
+                    inputProps={{min: '-50', max: '50'}}
                     variant='outlined'
                     InputLabelProps={{
                     shrink: true,
@@ -37,7 +59,7 @@ export default function StepperIncrementContent () {
                 <TextField
                     id="increment-field-Y"
                     type="number"
-                    inputProps={{min: '0', max: '50'}}
+                    inputProps={{min: '-50', max: '50'}}
                     variant='outlined'
                     InputLabelProps={{
                     shrink: true,
@@ -48,7 +70,29 @@ export default function StepperIncrementContent () {
                 <TextField
                     id="increment-field-Z"
                     type="number"
-                    inputProps={{min: '0', max: '50'}}
+                    inputProps={{min: '-50', max: '50'}}
+                    variant='outlined'
+                    InputLabelProps={{
+                    shrink: true,
+                    }}
+                /> 
+                </Paper>
+                <Paper style={styles.textfield}>
+                <TextField
+                    id="increment-field-B"
+                    type="number"
+                    inputProps={{min: '-50', max: '50'}}
+                    variant='outlined'
+                    InputLabelProps={{
+                    shrink: true,
+                    }}
+                /> 
+                </Paper>
+                <Paper style={styles.textfield}>
+                <TextField
+                    id="increment-field-C"
+                    type="number"
+                    inputProps={{min: '-50', max: '50'}}
                     variant='outlined'
                     InputLabelProps={{
                     shrink: true,
@@ -61,29 +105,51 @@ export default function StepperIncrementContent () {
                     <Button
                         variant="contained"
                         color="primary"
-                        style={styles.button}
-                        endIcon={<ChevronRight style={styles.icon} />}
+                        className={classes.button}
                     >
                         Send X
                     </Button>
                     <Button
                         variant="contained"
                         color="primary"
-                        style={styles.button}
-                        endIcon={<ChevronRight style={styles.icon} />}
+                        className={classes.button}
                     >
                         Send Y
                     </Button>
                     <Button
                         variant="contained"
                         color="primary"
-                        style={styles.button}
-                        endIcon={<ChevronRight style={styles.icon} />}
+                        className={classes.button}
                     >
                         Send Z
                     </Button>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        className={classes.button}
+                    >
+                        Send B
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        className={classes.button}
+                    >
+                        Send C
+                    </Button>
                 </Grid>
                 <Grid item style={{display: 'flex', flex: 1, justifyContent: 'space-around'}} >
+                    <Grid item style={{flex: 1}}>
+
+                    </Grid>
+                    <Grid item style={{display: 'flex', flex: 1, justifyContent: 'space-around', alignItems: 'center'}}>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        style={{height: '6vh', fontSize: '1.5vw', backgroundColor: 'rgb(15, 76, 117)', color: 'rgb(187, 225, 250)'}}
+                    >
+                        <Refresh style={styles.icon} />
+                    </Button>
                     <Button
                         variant="contained"
                         color="primary"
@@ -92,6 +158,7 @@ export default function StepperIncrementContent () {
                     >
                         Send All
                     </Button>
+                    </Grid>
                 </Grid>
             </Grid>
 
