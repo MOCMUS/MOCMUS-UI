eb4f2543aec7456aa0d8313f4c8f3a5efd1fb552
mima2101 <mimault0221@hotmail.com>
2021-11-10 17:47:38 -0500

    fixed jog feedrate and machine info display

--- src/client/components/MachineInfoContent.js
+++ src/client/components/MachineInfoContent.js
@@ -12,11 +12,18 @@ export default function MachineInfoContent () {
     const [MposZ, setMposZ] = useState(sessionStorage.getItem('MposZ') ? sessionStorage.getItem('MposZ') : '0.000')
     const [MposB, setMposB] = useState(sessionStorage.getItem('MposB') ? sessionStorage.getItem('MposB') : '0.000')
     const [MposC, setMposC] = useState(sessionStorage.getItem('MposC') ? sessionStorage.getItem('MposC') : '0.000')
+
     const [WposX, setWposX] = useState(sessionStorage.getItem('WposX') ? sessionStorage.getItem('WposX') : '0.000')
     const [WposY, setWposY] = useState(sessionStorage.getItem('WposY') ? sessionStorage.getItem('WposY') : '0.000')
     const [WposZ, setWposZ] = useState(sessionStorage.getItem('WposZ') ? sessionStorage.getItem('WposZ') : '0.000')
     const [WposB, setWposB] = useState(sessionStorage.getItem('WposB') ? sessionStorage.getItem('WposB') : '0.000')
     const [WposC, setWposC] = useState(sessionStorage.getItem('WposC') ? sessionStorage.getItem('WposC') : '0.000')
+
+    const [WCOX, setWCOX] = useState(sessionStorage.getItem('WCOX') ? sessionStorage.getItem('WCOX') : '0.000')
+    const [WCOY, setWCOY] = useState(sessionStorage.getItem('WCOY') ? sessionStorage.getItem('WCOY') : '0.000')
+    const [WCOZ, setWCOZ] = useState(sessionStorage.getItem('WCOZ') ? sessionStorage.getItem('WCOZ') : '0.000')
+    const [WCOB, setWCOB] = useState(sessionStorage.getItem('WCOB') ? sessionStorage.getItem('WCOB') : '0.000')
+    const [WCOC, setWCOC] = useState(sessionStorage.getItem('WCOC') ? sessionStorage.getItem('WCOC') : '0.000')
 
     const [feedrate, setFeedrate] = useState(sessionStorage.getItem('feedrate') ? sessionStorage.getItem('feedrate') : '0')
     const [spindleSpeed, setSpindleSpeed] = useState(sessionStorage.getItem('spindle_speed') ? sessionStorage.getItem('spindle_speed') : '0')
@@ -58,7 +65,7 @@ export default function MachineInfoContent () {
         sessionStorage.setItem('spindle_speed', FSInfo[1])
       }
 
-      const WPosInfo = infoPositions.find(info => info.startsWith('WCO'))?.split(':')[1].split(',')
+      const WPosInfo = infoPositions.find(info => info.startsWith('WPos'))?.split(':')[1].split(',')
       if (WPosInfo) {
         setWposX(WPosInfo[0])
         sessionStorage.setItem('WposX', WPosInfo[0])
@@ -70,6 +77,20 @@ export default function MachineInfoContent () {
         sessionStorage.setItem('WposB', WPosInfo[3])
         setWposC(WPosInfo[4])
         sessionStorage.setItem('WposC', WPosInfo[4])
+      }
+
+      const WCOInfo = infoPositions.find(info => info.startsWith('WCO'))?.split(':')[1].split(',')
+      if (WCOInfo) {
+        setWCOX(WCOInfo[0])
+        sessionStorage.setItem('WCOX', WCOInfo[0])
+        setWCOY(WCOInfo[1])
+        sessionStorage.setItem('WCOY', WCOInfo[1])
+        setWCOZ(WCOInfo[2])
+        sessionStorage.setItem('WCOZ', WCOInfo[2])
+        setWCOB(WCOInfo[3])
+        sessionStorage.setItem('WCOB', WCOInfo[3])
+        setWCOC(WCOInfo[4])
+        sessionStorage.setItem('WCOC', WCOInfo[4])
       }
 
       const OvInfo = infoPositions.find(info => info.startsWith('Ov'))?.split(':')[1].split(',')
@@ -102,7 +123,8 @@ export default function MachineInfoContent () {
     const inputStyle = {
         fontSize: 30,
         fontFamily: 'sans-serif',
-        height: '5vh'
+        height: '0.8vh',
+        textAlign: 'center'
       }
 
       const axeCardStyle = {
@@ -144,7 +166,7 @@ export default function MachineInfoContent () {
                         </Grid>
                         <Grid container style={{display: 'flex', flex: 2, flexDirection: 'column'}} >
                             <Grid item style={{display: 'flex', flex: 1, justifyContent: 'center'}} >
-                                <Paper style={{textAlign: 'center', alignSelf: 'center', fontSize: '3vh', padding: 2, backgroundColor: 'rgb(187, 225, 250)'}}><b style={{marginLeft: 10, marginRight: 10}}>Home</b></Paper>
+                                <Paper style={{textAlign: 'center', alignSelf: 'center', fontSize: '3vh', padding: 2, backgroundColor: 'rgb(187, 225, 250)'}}><b style={{marginLeft: 10, marginRight: 10}}>MPos</b></Paper>
                             </Grid>
                             <Grid item style={{display: 'flex', flex: 1, justifyContent: 'center', padding: 2}} >
                                 <Paper style={{textAlign: 'center', alignSelf: 'center', padding: 2}}>
@@ -152,8 +174,7 @@ export default function MachineInfoContent () {
                                         id="home-X"
                                         type="text"
                                         value={MposX}
-                                        inputProps={{readOnly: true}}
-                                        InputProps={{style: inputStyle}}
+                                        inputProps={{readOnly: true, style: inputStyle}}
                                         variant='outlined'
                                     />
                                 </Paper>
@@ -164,8 +185,7 @@ export default function MachineInfoContent () {
                                         id="home-Y"
                                         type="text"
                                         value={MposY}
-                                        inputProps={{readOnly: true}}
-                                        InputProps={{style: inputStyle}}
+                                        inputProps={{readOnly: true, style: inputStyle}}
                                         variant='outlined'
                                     />
                                 </Paper>
@@ -176,8 +196,7 @@ export default function MachineInfoContent () {
                                         id="home-Z"
                                         type="text"
                                         value={MposZ}
-                                        inputProps={{readOnly: true}}
-                                        InputProps={{style: inputStyle}}
+                                        inputProps={{readOnly: true, style: inputStyle}}
                                         variant='outlined'
                                     />
                                 </Paper>
@@ -188,8 +207,7 @@ export default function MachineInfoContent () {
                                         id="home-B"
                                         type="text"
                                         value={MposB}
-                                        inputProps={{readOnly: true}}
-                                        InputProps={{style: inputStyle}}
+                                        inputProps={{readOnly: true, style: inputStyle}}
                                         variant='outlined'
                                     />
                                 </Paper>
@@ -200,8 +218,7 @@ export default function MachineInfoContent () {
                                         id="home-C"
                                         type="text"
                                         value={MposC}
-                                        inputProps={{readOnly: true}}
-                                        InputProps={{style: inputStyle}}
+                                        inputProps={{readOnly: true, style: inputStyle}}
                                         variant='outlined'
                                     />
                                 </Paper>
@@ -209,7 +226,7 @@ export default function MachineInfoContent () {
                         </Grid>
                         <Grid container style={{display: 'flex', flex: 2, flexDirection: 'column'}} >
                             <Grid item style={{display: 'flex', flex: 1, justifyContent: 'center'}} >
-                                <Paper style={{textAlign: 'center', alignSelf: 'center', fontSize: '3vh', padding: 2, backgroundColor: 'rgb(187, 225, 250)'}}><b style={{marginLeft: 10, marginRight: 10}}>WCS</b></Paper>
+                                <Paper style={{textAlign: 'center', alignSelf: 'center', fontSize: '3vh', padding: 2, backgroundColor: 'rgb(187, 225, 250)'}}><b style={{marginLeft: 10, marginRight: 10}}>WPos</b></Paper>
                             </Grid>
                             <Grid item style={{display: 'flex', flex: 1, justifyContent: 'center', padding: 2}} >
                                 <Paper style={{textAlign: 'center', alignSelf: 'center', padding: 2}}>
@@ -217,8 +234,7 @@ export default function MachineInfoContent () {
                                         id="wcs-X"
                                         type="text"
                                         value={WposX}
-                                        inputProps={{readOnly: true}}
-                                        InputProps={{style: inputStyle}}
+                                        inputProps={{readOnly: true, style: inputStyle}}
                                         variant='outlined'
                                     />
                                 </Paper>
@@ -229,8 +245,7 @@ export default function MachineInfoContent () {
                                         id="wcs-Y"
                                         type="text"
                                         value={WposY}
-                                        inputProps={{readOnly: true}}
-                                        InputProps={{style: inputStyle}}
+                                        inputProps={{readOnly: true, style: inputStyle}}
                                         variant='outlined'
                                     />
                                 </Paper>
@@ -241,8 +256,7 @@ export default function MachineInfoContent () {
                                         id="wcs-Z"
                                         type="text"
                                         value={WposZ}
-                                        inputProps={{readOnly: true}}
-                                        InputProps={{style: inputStyle}}
+                                        inputProps={{readOnly: true, style: inputStyle}}
                                         variant='outlined'
                                     />
                                 </Paper>
@@ -253,8 +267,7 @@ export default function MachineInfoContent () {
                                         id="wcs-B"
                                         type="text"
                                         value={WposB}
-                                        inputProps={{readOnly: true}}
-                                        InputProps={{style: inputStyle}}
+                                        inputProps={{readOnly: true, style: inputStyle}}
                                         variant='outlined'
                                     />
                                 </Paper>
@@ -265,8 +278,67 @@ export default function MachineInfoContent () {
                                         id="wcs-C"
                                         type="text"
                                         value={WposC}
-                                        inputProps={{readOnly: true}}
-                                        InputProps={{style: inputStyle}}
+                                        inputProps={{readOnly: true, style: inputStyle}}
+                                        variant='outlined'
+                                    />
+                                </Paper>
+                            </Grid>
+                        </Grid>
+                        <Grid container style={{display: 'flex', flex: 2, flexDirection: 'column'}} >
+                            <Grid item style={{display: 'flex', flex: 1, justifyContent: 'center'}} >
+                                <Paper style={{textAlign: 'center', alignSelf: 'center', fontSize: '3vh', padding: 2, backgroundColor: 'rgb(187, 225, 250)'}}><b style={{marginLeft: 10, marginRight: 10}}>WCO</b></Paper>
+                            </Grid>
+                            <Grid item style={{display: 'flex', flex: 1, justifyContent: 'center', padding: 2}} >
+                                <Paper style={{textAlign: 'center', alignSelf: 'center', padding: 2}}>
+                                    <TextField
+                                        id="wco-X"
+                                        type="text"
+                                        value={WCOX}
+                                        inputProps={{readOnly: true, style: inputStyle}}
+                                        variant='outlined'
+                                    />
+                                </Paper>
+                            </Grid>
+                            <Grid item style={{display: 'flex', flex: 1, justifyContent: 'center', padding: 2}} >
+                                <Paper style={{textAlign: 'center', alignSelf: 'center', padding: 2}}>
+                                    <TextField
+                                        id="wco-Y"
+                                        type="text"
+                                        value={WCOY}
+                                        inputProps={{readOnly: true, style: inputStyle}}
+                                        variant='outlined'
+                                    />
+                                </Paper>
+                            </Grid>
+                            <Grid item style={{display: 'flex', flex: 1, justifyContent: 'center', padding: 2}} >
+                                <Paper style={{textAlign: 'center', alignSelf: 'center', padding: 2}}>
+                                    <TextField
+                                        id="wco-Z"
+                                        type="text"
+                                        value={WCOZ}
+                                        inputProps={{readOnly: true, style: inputStyle}}
+                                        variant='outlined'
+                                    />
+                                </Paper>
+                            </Grid>
+                            <Grid item style={{display: 'flex', flex: 1, justifyContent: 'center', padding: 2}} >
+                                <Paper style={{textAlign: 'center', alignSelf: 'center', padding: 2}}>
+                                    <TextField
+                                        id="wco-B"
+                                        type="text"
+                                        value={WCOB}
+                                        inputProps={{readOnly: true, style: inputStyle}}
+                                        variant='outlined'
+                                    />
+                                </Paper>
+                            </Grid>
+                            <Grid item style={{display: 'flex', flex: 1, justifyContent: 'center', padding: 2}} >
+                                <Paper style={{textAlign: 'center', alignSelf: 'center', padding: 2}}>
+                                    <TextField
+                                        id="wco-C"
+                                        type="text"
+                                        value={WCOC}
+                                        inputProps={{readOnly: true, style: inputStyle}}
                                         variant='outlined'
                                     />
                                 </Paper>
